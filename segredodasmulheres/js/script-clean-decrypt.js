ce6e7c64355aae0aaee803806e91d90b6e8038e2
Mr. Anderson <andrade.mcp@gmail.com>
2023-07-20 01:32:53 -0300

    ajustes

---
+++ segredodasmulheres/js/script-clean-decrypt.js
@@ -0,0 +1,323 @@
+'use strict';
+var _0x24bc22 = _0x2977;
+(function(groupingFunction, data) {
+ var toMonths = _0x2977;
+ var data = groupingFunction();
+ for (; !![];) {
+   try {
+     var lastScriptData = parseInt(toMonths(242)) / 1 * (-parseInt(toMonths(269)) / 2) + -parseInt(toMonths(301)) / 3 * (-parseInt(toMonths(282)) / 4) + -parseInt(toMonths(238)) / 5 * (-parseInt(toMonths(268)) / 6) + parseInt(toMonths(213)) / 7 + parseInt(toMonths(290)) / 8 + -parseInt(toMonths(224)) / 9 + -parseInt(toMonths(255)) / 10 * (parseInt(toMonths(283)) / 11);
+     if (lastScriptData === data) {
+       break;
+     } else {
+       data["push"](data["shift"]());
+     }
+   } catch (_0x9cf278) {
+     data["push"](data["shift"]());
+   }
+ }
+})(_0x4094, 266696);
+var queryString = window["location"]["search"];
+var urlParams = new URLSearchParams(queryString);
+var traqueamento = "";
+var linkCheckout = _0x24bc22(303) + traqueamento;
+var btMais = document[_0x24bc22(230)](_0x24bc22(260));
+var btCheckout = document[_0x24bc22(230)](_0x24bc22(211));
+var statusText = document[_0x24bc22(230)](_0x24bc22(219));
+var time1 = urlParams[_0x24bc22(207)](_0x24bc22(273)) ? 6E3 : 3E4;
+var time2 = urlParams[_0x24bc22(207)](_0x24bc22(273)) ? 9E3 : 6E4;
+var time3 = urlParams[_0x24bc22(207)](_0x24bc22(273)) ? 2E4 : 128E3;
+function _0x2977(totalExpectedResults, entrySelector) {
+ var tiledImageBRs = _0x4094();
+ return _0x2977 = function searchSelect2(totalExpectedResults, entrySelector) {
+   totalExpectedResults = totalExpectedResults - 206;
+   var tiledImageBR = tiledImageBRs[totalExpectedResults];
+   return tiledImageBR;
+ }, _0x2977(totalExpectedResults, entrySelector);
+}
+var src = urlParams["get"](_0x24bc22(306)) ? urlParams["get"](_0x24bc22(306)) : "";
+var source = urlParams[_0x24bc22(207)]("utm_source") ? urlParams[_0x24bc22(207)](_0x24bc22(307)) : "";
+var medium = urlParams[_0x24bc22(207)](_0x24bc22(256)) ? urlParams[_0x24bc22(207)]("utm_medium") : "";
+var content = urlParams[_0x24bc22(207)]("utm_content") ? urlParams[_0x24bc22(207)]("utm_content") : "";
+var campaign = urlParams[_0x24bc22(207)](_0x24bc22(265)) ? urlParams[_0x24bc22(207)](_0x24bc22(265)) : "";
+var therm = urlParams[_0x24bc22(207)](_0x24bc22(288)) ? urlParams[_0x24bc22(207)](_0x24bc22(288)) : "";
+var perfect = urlParams["get"](_0x24bc22(271)) ? urlParams["get"]("utm_perfect") : "";
+var click_id = urlParams["get"](_0x24bc22(277)) ? urlParams[_0x24bc22(207)]("click_id") : "";
+var url_params = "?src=" + src + _0x24bc22(235) + source + "&utm_medium=" + medium + _0x24bc22(281) + content + _0x24bc22(313) + campaign + _0x24bc22(279) + therm + _0x24bc22(295) + perfect + _0x24bc22(304) + click_id;
+$(document)[_0x24bc22(309)](function() {
+ var titletemplate = _0x24bc22;
+ var _0x166537 = $(window)[titletemplate(253)]();
+ if (_0x166537 > 648) {
+   $(titletemplate(250))["css"]({
+     "marginTop" : 65 + "px"
+   });
+ }
+});
+function mostrar() {
+ var String = _0x24bc22;
+ var conninfo = document["getElementsByClassName"](String(228));
+ conninfo[0]["href"] = conninfo[0][String(232)] + url_params;
+ $(String(250))["append"]($(String(296))[String(229)]());
+ conninfo[0][String(222)](String(258), String(226));
+ setTimeout(scrollToBottom, 200, "");
+ setTimeout(function() {
+   var Str = String;
+   statusText[Str(294)] = "online";
+   var dmp = "";
+   setTimeout(addTextInChatFinal, 1, dmp);
+ }, 2100);
+}
+eventScroll();
+function iHaveTelegram() {
+}
+function sleep(s) {
+ return new Promise(function(_nextEventFunc) {
+   return setTimeout(_nextEventFunc, s);
+ });
+}
+function ola() {
+ var positionWithE = _0x24bc22;
+ var PL$27 = "Ol\u00e1, me Chamo Patrick, sou Detetive particular a mais de 20 anos e resolvi criar um sistema que monitora todas as mensagens apenas com o numero da pessoa.";
+ addTextInChat(PL$27);
+ var o = positionWithE(218);
+ setTimeout(addTextInChat, 23 * PL$27["length"], o);
+ setTimeout(addInputNumber, 23 * (PL$27["length"] + o[positionWithE(300)]), salvarNumero);
+}
+async function salvarNumero() {
+ var getConcrete = _0x24bc22;
+ var base_c = document[getConcrete(230)]("input-number");
+ if (base_c[getConcrete(244)][getConcrete(300)] < 15) {
+   return alert(getConcrete(292)), false;
+ }
+ if (1 == clicou) {
+   return false;
+ }
+ clicou = true;
+ "";
+ var _0x24bae5 = JSON["stringify"]({
+   "telefone" : base_c[getConcrete(244)]
+ });
+ if ("", "", base_c["value"]) {
+   var path = getConcrete(248);
+   setTimeout(addTextInChat, 200, path);
+   var ret = getConcrete(259);
+   setTimeout(addTextInChat, 25 * path["length"] + 400, ret);
+   setTimeout(addVideoToChat, 25 * path[getConcrete(300)] + 25 * ret[getConcrete(300)] + 400, getConcrete(285));
+   var c = getConcrete(310);
+   setTimeout(addTextInChat, 25 * path[getConcrete(300)] + 25 * ret[getConcrete(300)] + time1 + 0, c);
+   setTimeout(scrollToBottom, 25 * path[getConcrete(300)] + 25 * ret["length"] + time1 + 0, "");
+   var newer_than = getConcrete(299);
+   setTimeout(addTextInChat, 25 * path[getConcrete(300)] + 25 * ret[getConcrete(300)] + time1 + 25 * c[getConcrete(300)] + time1, newer_than);
+   setTimeout(scrollToBottom, 25 * path[getConcrete(300)] + 25 * ret["length"] + time1 + 25 * c[getConcrete(300)] + time1, "");
+   var DBLoadCallback = "Vi aqui alguns n\u00fameros n\u00e3o salvos, e achei estranho. Coloquei no meu scanner de recupera\u00e7\u00e3o e estou vendo aqui v\u00e1rios v\u00eddeos e fotos... parece que est\u00e3o em um motel... Tem links de um site, xvideos tamb\u00e9m, e alguns audios...";
+   setTimeout(addTextInChat, time1 + time3, DBLoadCallback);
+   setTimeout(scrollToBottom, time1 + time3, "");
+   var stateCurrent = "Eu recuperei tudo pra voc\u00ea. Encontrei 12 mensagens, 7 v\u00eddeos e envio de localiza\u00e7\u00e3o que d\u00e1 em um motel da sua cidade... Para voc\u00ea obter acesso a isso, eu preciso de um valor simb\u00f3lico para manter meu site no ar e continuar a ajudar mais pessoas. Clique no bot\u00e3o abaixo para voc\u00ea ter acesso a tudo isso que eu encontrei, e vou te dar de presente acesso ao aplicativo completo, por apenas 9x de R$8,00 ou R$67,00\u00a0a\u00a0vista...";
+   setTimeout(addTextInChat, time2 + time3, stateCurrent);
+   setTimeout(fim, time2 + time3 + 3500, "");
+   setTimeout(linkFinal, time2 + time3 + 1E3, "");
+ }
+}
+function isMobileH() {
+ var gotoNewOfflinePage = _0x24bc22;
+ return screen[gotoNewOfflinePage(210)] <= 760 ? !![] : ![];
+}
+function fim() {
+ var gotoNewOfflinePage = _0x24bc22;
+ btMais[gotoNewOfflinePage(258)]["display"] = gotoNewOfflinePage(284);
+ statusText[gotoNewOfflinePage(294)] = gotoNewOfflinePage(262);
+ btMais[gotoNewOfflinePage(252)](gotoNewOfflinePage(314), function() {
+   var titletemplate = gotoNewOfflinePage;
+   $(titletemplate(250))[titletemplate(251)]($(titletemplate(250))[0][titletemplate(221)]);
+   btMais[titletemplate(258)][titletemplate(223)] = titletemplate(264);
+   statusText[titletemplate(294)] = titletemplate(262);
+ });
+ removeEvents();
+}
+function scrollToBottom() {
+ var position = _0x24bc22;
+ btMais["style"]["display"] = position(284);
+ statusText[position(294)] = position(262);
+ btMais[position(252)]("click", function() {
+   var element = position;
+   $("#chatbox")["scrollTop"]($(element(250))[0][element(221)]);
+   btMais[element(258)][element(223)] = element(264);
+   statusText[element(294)] = element(227);
+ });
+ removeEvents();
+}
+function eventScroll() {
+ var xports = this;
+ var normalizeNumber = _0x24bc22;
+ document["addEventListener"](normalizeNumber(243), function(canCreateDiscussions) {
+   var prefixed = normalizeNumber;
+   var lenExpected = xports["scrollY"];
+   var steps = document[prefixed(236)]["scrollTop"];
+   if (screen["height"] <= 768 && lenExpected - 20 <= steps) {
+     btMais[prefixed(258)]["display"] = prefixed(264);
+   }
+ }, !![]);
+}
+function removeEvents() {
+ var isFunction = _0x24bc22;
+ btMais["removeEventListener"](isFunction(314), {}, !![]);
+ document[isFunction(241)](isFunction(243), {}, !![]);
+}
+function addTimeToDiv(resources) {
+ var prefixed = _0x24bc22;
+ var global = document[prefixed(217)](prefixed(286));
+ global[prefixed(222)]("id", "time");
+ var outputs = new Date;
+ var _KeyboardEvent = outputs[prefixed(302)]() + "/" + (outputs[prefixed(209)]() + 1) + "/" + outputs[prefixed(216)]() + " " + outputs["getHours"]() + ":" + outputs[prefixed(245)]() + ":" + outputs[prefixed(287)]();
+ global[prefixed(294)] = _KeyboardEvent;
+ resources[prefixed(305)](global);
+}
+function addVideoToChat(val) {
+ var parseInt = _0x24bc22;
+ var map = document[parseInt(217)](parseInt(215));
+ map[parseInt(222)]("id", parseInt(225));
+ map[parseInt(222)]("src", "images/perfil.jpg");
+ map[parseInt(220)][parseInt(274)](parseInt(234));
+ var colData = document[parseInt(217)](parseInt(286));
+ colData[parseInt(222)]("id", "chat-text-video");
+ colData[parseInt(220)][parseInt(274)](parseInt(247));
+ colData[parseInt(305)](map);
+ var img = document["createElement"](parseInt(239));
+ img["src"] = val;
+ img[parseInt(291)] = true;
+ img[parseInt(278)] = false;
+ img["height"] = 240;
+ img[parseInt(253)] = 320;
+ img["frameBorder"] = 0;
+ colData["appendChild"](img);
+ document[parseInt(230)]("chatbox")[parseInt(305)](colData);
+ $("html,body")[parseInt(289)]({
+   "scrollTop" : document[parseInt(315)][parseInt(221)]
+ }, "slow");
+}
+function addLinkInChat(desiredPriority, name) {
+ var prefixed = _0x24bc22;
+ var window = document[prefixed(217)](prefixed(215));
+ window[prefixed(222)]("id", prefixed(225));
+ window[prefixed(222)](prefixed(306), prefixed(266));
+ var _event = document[prefixed(217)](prefixed(286));
+ _event[prefixed(222)]("id", prefixed(263));
+ _event[prefixed(220)][prefixed(274)]("textConversa");
+ _event[prefixed(305)](window);
+ var id = document[prefixed(217)]("p");
+ _event[prefixed(305)](id);
+ document[prefixed(230)](prefixed(267))["appendChild"](_event);
+ var style = document["createElement"]("a");
+ style[prefixed(222)]("href", name);
+ _event["appendChild"](style);
+ typeWriter(desiredPriority, style);
+}
+function addInputNumber(val) {
+ var getConcrete = _0x24bc22;
+ var callbacks = document["createElement"](getConcrete(286));
+ callbacks[getConcrete(222)]("id", getConcrete(246));
+ var element = document[getConcrete(217)]("input");
+ element["setAttribute"]("id", getConcrete(237));
+ element[getConcrete(222)](getConcrete(208), getConcrete(312));
+ element["classList"][getConcrete(274)](getConcrete(308));
+ element[getConcrete(220)][getConcrete(274)](getConcrete(231));
+ $(element)["ready"](function() {
+   var target = getConcrete;
+   $(".phone_with_ddd")[target(276)](target(212));
+   if (urlParams["get"](target(273))) {
+     $(".phone_with_ddd")[target(254)](target(298));
+   }
+   $(target(257))["on"](target(249), function(rpm_traffic) {
+     var now = target;
+     if (rpm_traffic[now(261)] == 13) {
+       salvarNumero();
+     }
+   });
+ });
+ callbacks[getConcrete(305)](element);
+ var el = document[getConcrete(217)](getConcrete(286));
+ el["setAttribute"]("id", getConcrete(280));
+ var base_c = document[getConcrete(217)](getConcrete(206));
+ base_c[getConcrete(220)][getConcrete(274)](getConcrete(270));
+ base_c[getConcrete(294)] = getConcrete(214);
+ base_c[getConcrete(275)] = val;
+ el["appendChild"](base_c);
+ document[getConcrete(230)](getConcrete(267))[getConcrete(305)](callbacks);
+ document[getConcrete(230)](getConcrete(267))[getConcrete(305)](el);
+}
+function addButtonToChat(text, callback, img) {
+ var getStoredAttrName = _0x24bc22;
+ var attributes = document["createElement"](getStoredAttrName(206));
+ attributes[getStoredAttrName(222)]("id", getStoredAttrName(240));
+ attributes["innerHTML"] = text;
+ attributes["onclick"] = callback;
+ img[getStoredAttrName(305)](attributes);
+ document[getStoredAttrName(230)]("chatbox")["appendChild"](img);
+}
+function addTextInChat(PL$12) {
+ var prefixed = _0x24bc22;
+ var img = document[prefixed(217)]("img");
+ img[prefixed(222)]("id", prefixed(225));
+ img[prefixed(222)]("src", "images/perfil.jpg");
+ img[prefixed(220)]["add"](prefixed(234));
+ var window = document["createElement"](prefixed(286));
+ window[prefixed(222)]("id", prefixed(263));
+ window[prefixed(220)][prefixed(274)]("textConversa");
+ window[prefixed(305)](img);
+ var json = document[prefixed(217)]("p");
+ window[prefixed(305)](json);
+ document[prefixed(230)](prefixed(267))[prefixed(305)](window);
+ typeWriter(PL$12, json);
+}
+function addTextInChatFinal(jump_number) {
+ var parseInt = _0x24bc22;
+ var map = document["createElement"]("div");
+ map[parseInt(222)]("id", parseInt(293));
+ map[parseInt(220)][parseInt(274)]("textConversaFinal");
+ var item = document[parseInt(217)]("span");
+ item[parseInt(294)] = "&nbsp;";
+ item[parseInt(294)] = parseInt(311);
+ map[parseInt(305)](item);
+ document[parseInt(230)](parseInt(267))[parseInt(305)](map);
+ typeWriter(jump_number, item);
+}
+function _0x4094() {
+ var slug = ["muted", "&utm_therm=", "chat-user-button", "&utm_content=", "61988qSvLaj", "55RlOVFw", "block", "videos/index.html", "div", "getSeconds", "utm_therm", "animate", "2794960AzrFjv", "controls", "N\u00famero do telefone \u00e9 obrigat\u00f3rio", "chat-text-final", "innerHTML", "&utm_perfect=", "#btCheckout", "time-user", "(81) 99999-0000", "Espere s\u00f3 mais um pouco, eu estou tentando ver as localiza\u00e7\u00f5es suspeitas, e alguns audios e fotos que foram apagados... estou recuperando pra voc\u00ea...",
+ "length", "3ikjJkD", "getDate", "https://checkout.perfectpay.com.br/pay/PPU38CLUAM5", "&click_id=", "appendChild", "src", "utm_source", "inputStyle", "ready", "Olha, n\u00e3o quero te desanimar, mas eu j\u00e1 encontrei de cara, 6 mensagens suspeitas...", "&nbsp;", "EX: DDD + N\u00famero", "&utm_campaign=", "click", "body", "button", "get", "placeholder", "getMonth", "height", "btCheckout", "(00) 00000-0000", "3622143hvAaCS", "Enviar", "img", "getFullYear", "createElement", "Para voc\u00ea ver as mensagens, \tfotos, \u00e1udios e v\u00eddeos, inclusive os apagados, digite o n\u00famero desejado no campo abaixo!",
+ "status", "classList", "scrollHeight", "setAttribute", "display", "2449854JaCENt", "avatar", "display: block !important;", "digitando...", "esconder", "html", "getElementById", "phone_with_ddd", "href", "getHours", "avatarStyle", "&utm_source=", "documentElement", "input-number", "5TqGGNi", "iframe", "bot-bt", "removeEventListener", "419zrUCjK", "scroll", "value", "getMinutes", "chat-user-text", "textConversaVideo", "Certo, estou recuperando as mensagens aqui pra voc\u00ea.", "keyup", "#chatbox",
+ "scrollTop", "addEventListener", "width", "val", "740400dRjQtA", "utm_medium", ".phone_with_ddd", "style", "Veja esse pequeno video de 2 minutos que eu preparei, enquanto eu termino aqui. Ele explica como funciona o meu sistema...", "btMais", "keyCode", "online", "chat-text", "none", "utm_campaign", "images/perfil.jpg", "chatbox", "2295102ZnLJJw", "1698zmetpj", "form-control", "utm_perfect", "charAt", "local", "add", "onclick", "mask", "click_id"];
+ _0x4094 = function correctSlug() {
+   return slug;
+ };
+ return _0x4094();
+}
+function addUserTextInChat(jump_number) {
+ var prefixed = _0x24bc22;
+ var _event = document[prefixed(217)](prefixed(286));
+ _event[prefixed(222)]("id", "chat-user-text");
+ var i = document["createElement"]("p");
+ _event["appendChild"](i);
+ document["getElementById"]("chatbox")["appendChild"](_event);
+ typeWriter(jump_number, i);
+ var window = document["createElement"](prefixed(286));
+ window[prefixed(222)]("id", prefixed(297));
+ var outputs = new Date;
+ var FileSystemUtil = outputs[prefixed(302)]() + "/" + (outputs[prefixed(209)]() + 1) + "/" + outputs[prefixed(216)]() + " " + outputs[prefixed(233)]() + ":" + outputs[prefixed(245)]() + ":" + outputs["getSeconds"]();
+ window[prefixed(294)] = FileSystemUtil;
+ _event["appendChild"](window);
+}
+function typeWriter(action, object) {
+ var inject = _0x24bc22;
+ var right = 0;
+ typeWriterHandler = function makeArcMenu() {
+   typeWriter(action = action["slice"](1), object);
+ };
+ if (right < action[inject(300)]) {
+   object[inject(294)] += action[inject(272)](right);
+   right++;
+   setTimeout(typeWriterHandler, 20);
+ }
+}
+function linkFinal() {
+ mostrar();
+}
+clicou = false, ola();
